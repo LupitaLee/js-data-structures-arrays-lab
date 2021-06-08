7442fbed5db8b9c8530fc8e51b684f1dbbd70595
Lupita <lupitarivera8899@gmail.com>
2021-06-08 15:36:26 -0500

    commit tree 4ff04ce8927b7665c6ca2e916b4c60eceeb42820

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"];
+
+
+function 
+destructivelyAppendDriver(name){
+    drivers.push(name);
+    return drivers
+}
+
+function
+destructivelyPrependDriver(name){
+    drivers.unshift(name)
+    return drivers
+}
+
+function
+destructivelyRemoveLastDriver(){
+    drivers.pop()
+    return drivers
+}
+
+function
+destructivelyRemoveFirstDriver(){
+    drivers.shift()
+    return drivers
+}
+
+function 
+appendDriver(name){
+   const newdrivers = [...drivers, name]
+   return newdrivers
+}
+
+function
+prependDriver(name){
+    const newdrivers = [name, ...drivers]
+   return newdrivers
+}
+
+function
+removeLastDriver(){
+    const newdrivers = drivers.slice(0,-1)
+    return newdrivers
+}
+
+function 
+removeFirstDriver(){
+    const newdrivers = drivers.slice(1)
+    return newdrivers
+}
